6b0bfa6f5893096ce2cc468e5f214505b62e91c3
BertaVR <bverges@cifpfbmoll.eu>
2021-02-23 12:21:25 +0100

    fix(he aññadido un stream para que no entren nulos, creo que se puede refactorizar)

--- stockx/src/main/java/edu/pingpong/domain/criteria/AndCriteria.java
+++ stockx/src/main/java/edu/pingpong/domain/criteria/AndCriteria.java
@@ -3,7 +3,6 @@ package edu.pingpong.domain.criteria;
 import edu.pingpong.domain.item.Item;
 import edu.pingpong.domain.item.Offer;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
--- stockx/src/main/java/edu/pingpong/domain/criteria/MaxBid.java
+++ stockx/src/main/java/edu/pingpong/domain/criteria/MaxBid.java
@@ -6,6 +6,7 @@ import edu.pingpong.domain.item.Offer;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MaxBid implements Criteria{
 
@@ -22,12 +23,12 @@ public class MaxBid implements Criteria{
         Offer maxBidOffer = bids.checkCriteria(sneaker).stream().filter(o -> o!=null).
                 max(Comparator.comparing(o->o.value())).orElse(null);
 
-        /* No sé, este último orElse me parece peligroso porque no quiero valores nulos, lo he tenido que poner porque
-         ** si no los objetos eran de clase Optional y no es lo que pide mi lista, creo que tendré que refactorizar.
-         ** El filtro de nulos tampoco sé qué tan útil es, probablemente lo borraré
-         */
 
         maxBid.add(maxBidOffer);
+
+        maxBid = maxBid.stream().filter(o -> o!=null) //Aquí quito los nulos
+                .collect(Collectors.toList());
+
 
         return maxBid;
     }
